191e19caccb14f8be19550f48480a593b7ff4334
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-05-06 11:13:08 -0500

    feat: Update nursing mental state exams page with exam cards

--- src/app/nursing/components/exam-card/exam-card.component.ts
+++ src/app/nursing/components/exam-card/exam-card.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { ExamEntity } from '../../models/exam.entity';
+import { PatientsEntity } from '../../models/patients.entity';
+import { ExaminersEntity } from '../../models/examiners.entity';
+import { MentalStateExamsEntity } from '../../models/mental-state-exams.entity';
 
 @Component({
   selector: 'app-exam-card',
--- src/app/nursing/pages/page-mental-state-exams/page-mental-state-exams.component.ts
+++ src/app/nursing/pages/page-mental-state-exams/page-mental-state-exams.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { PatientsApiService } from '../../services/patients-api.service';
 import { ExaminersService } from '../../services/examiners.service';
 import { MentalStateExamsApiService } from '../../services/mental-state-exams-api.service';
@@ -16,7 +16,6 @@ export class PageMentalStateExamsComponent implements OnInit {
   constructor(private patientsApi: PatientsApiService, private examinersApi: ExaminersService, private mentalStateExamsApi: MentalStateExamsApiService) {
   }
 
-
   public patients: Array<PatientsEntity> = [];
   public examiners: Array<ExaminersEntity> = [];
   public exams: Array<ExamEntity> = [];
@@ -26,6 +25,7 @@ export class PageMentalStateExamsComponent implements OnInit {
     this.getPatients();
     this.getExaminers();
     this.getMentalStateExams();
+    this.generateExam();
   }
 
   getPatients(): void {
@@ -46,6 +46,24 @@ export class PageMentalStateExamsComponent implements OnInit {
     this.mentalStateExamsApi.getAll().subscribe((mentalStateExams: any) => {
       this.mentalStateExams = mentalStateExams;
       console.log(this.mentalStateExams);
+      this.generateExam();
+    });
+    console.log(this.mentalStateExams);
+  }
+
+  generateExam() {
+    this.mentalStateExams.map((mentalExam) => {
+      let patient: any = this.patients.find((patient) => patient.id === mentalExam.patientId);
+      let examiner: any = this.examiners.find((examiner) => examiner.id === mentalExam.examinerId);
+      let exam = {} as ExamEntity;
+      exam.photoUrl = patient.photoUrl;
+      exam.patientName = patient.firstName + ' ' + patient.lastName;
+      exam.examinerName = examiner.firstName + ' ' + examiner.lastName;
+      exam.examDate = mentalExam.examDate;
+      exam.totalScore = mentalExam.registrationScore + mentalExam.languageScore + mentalExam.recallScore + mentalExam.registrationScore;
+      exam.nationalProviderIdentifier = examiner.nationalProviderIdentifier;
+      this.exams.push(exam);
+      console.log(exam);
     });
   }
 }
