d3c5e54e176b0afc7e747e0db4a4f3c63b5dc18d
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-05-06 09:13:40 -0500

    feat: Update base API service to use environment variables

--- src/app/shared/services/base-api.service.ts
+++ src/app/shared/services/base-api.service.ts
@@ -1,9 +1,61 @@
-import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
+import { catchError, Observable, retry, throwError } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class BaseApiService {
+export class BaseService<T> {
+  basePath: string = `${environment.baseURL}`;
+  resourceEndpoint: string = '/';
 
-  constructor() { }
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-type': 'application/json',
+    }),
+  };
+
+  constructor(private http: HttpClient) { }
+
+  handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred:', error.error.message);
+    }
+    return throwError(
+      () => new Error('Something happened with request, please try again later')
+    );
+  }
+
+  create(item: any): Observable<T> {
+    return this.http
+      .post<T>(this.resourcePath(), JSON.stringify(item), this.httpOptions)
+      .pipe(retry(1), catchError(this.handleError));
+  }
+
+  delete(id: any) {
+    return this.http
+      .delete(`${this.resourcePath()}/${id}`, this.httpOptions)
+      .pipe(retry(1), catchError(this.handleError));
+  }
+
+  update(id: any, item: any): Observable<T> {
+    return this.http
+      .put<T>(
+        `${this.resourcePath()}/${id}`,
+        JSON.stringify(item),
+        this.httpOptions
+      )
+      .pipe(retry(1), catchError(this.handleError));
+  }
+
+  getAll(): Observable<T> {
+    return this.http
+      .get<T>(this.resourcePath(), this.httpOptions)
+      .pipe(retry(1), catchError(this.handleError));
+  }
+
+  private resourcePath(): string {
+    return `${this.basePath}${this.resourceEndpoint}`;
+  }
 }
